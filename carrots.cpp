4b2a62585570245fa5619fadff5da5aa8a632065
shahenkosyan <shahen.qosyan@gmail.com>
2017-11-21 15:14:09 +0400

    Carrots accepted

---
+++ carrots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+  
+  for (int i = 0; i < x; ++i) {
+    string tmp;
+    cin >> tmp;
+  } 
+  
+  cout << y << endl;
+
+  return 0;
+}
