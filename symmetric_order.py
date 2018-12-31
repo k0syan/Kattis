411867f1816cb9c471ffd0424593a90cfb9b8549
Shahen Kosyan <shahen.qosyan@gmail.com>
2018-12-31 22:12:50 +0400

    symmetric_order.py

---
+++ symmetric_order.py
@@ -0,0 +1,34 @@
+if __name__ == "__main__":
+    n = int(input())
+    j = 1
+    while 1 == 1:
+        if n == 0:
+            break
+        unordered = [""] * n
+        ordered = [""] * n
+        for i in range(n):
+            unordered[i] = input()
+
+        for i in range(int(n / 2)):
+            ordered[i] = unordered[i * 2]
+            ordered[n - i - 1] = unordered[2 * i + 1]
+
+        if n % 2 == 1:
+            ordered[int(n / 2)] = unordered[n - 1]
+
+        print("SET", j)
+        for i in range(n):
+            print(ordered[i])
+
+        j += 1
+        n = int(input())
+
+# 7
+# Bo
+# Pat
+# Jean
+# Kevin
+# Claude
+# William
+# Marybeth
+# 0
