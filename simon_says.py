aa3556a3c896a587d4f8d3292c4a8900186e3a57
Shahen Kosyan <shahen.qosyan@gmail.com>
2019-01-07 21:59:49 +0400

    simon_says.py

--- simon_says.py
+++ simon_says.py
@@ -1,8 +1,8 @@
 t = int(input())
 while t != 0:
-    string = input().split()
-    if len(string) > 1 and string[0] == "simon" and string[1] == "says":
-        print(" ".join(string[2:]))
+    a = input().split()
+    if len(a) > 1 and a[0] == "Simon" and a[1] == "says":
+        print(" ".join(a[2:]))
     else:
         print()
     t -= 1
