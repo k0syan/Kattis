9d75e7c9c29e5d98d3d2fcb74eff5f1a63275ecd
Shahen Kosyan <shahen.qosyan@gmail.com>
2019-01-15 12:24:10 +0400

    reverse_rot.py

---
+++ reverse_rot.py
@@ -0,0 +1,29 @@
+if __name__ == "__main__":
+    letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ_.")
+    letters = [letter for letter in enumerate(letters)]
+    while 1 == 1:
+        tmp = input().split()
+        if len(tmp) == 1:
+            break
+        else:
+            c, word = int(tmp[0]), list(tmp[1])
+            fword = []
+            for w in word:
+                i = 0
+                for l in letters:
+                    if l[1] == w:
+                        i = l[0]
+                        break
+                i += c
+                if i >= len(letters):
+                    i -= len(letters)
+                let = ""
+                for l in letters:
+                    if l[0] == i:
+                        let = l[1]
+                        break
+                fword.append(let)
+            fword.reverse()
+            print("".join(fword))
+# 1 .DOT
+# 0
