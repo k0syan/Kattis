8296a3eeb7487c76939395e7287ad7d5172046bd
Shahen Kosyan <shahen.qosyan@gmail.com>
2019-01-15 17:23:54 +0400

    2.py

---
+++ 2.py
@@ -0,0 +1,48 @@
+def read_data():
+    with open("text.txt") as fp:
+        for line in fp:
+            features = [float(x) for x in line.split()]
+            print(features)
+
+
+def get_change(a, p):
+    t = a * 100
+    p = p * 100
+    diff = t - p
+    arr = [0, 0, 0, 0, 0, 0]
+
+    while diff != 0:
+        if diff >= 100:
+            diff -= 100
+            arr[5] += 1
+        elif diff >= 50:
+            diff -= 50
+            arr[4] += 1
+        elif diff >= 25:
+            diff -= 25
+            arr[3] += 1
+        elif diff >= 10:
+            diff -= 10
+            arr[2] += 1
+        elif diff >= 5:
+            diff -= 5
+            arr[1] += 1
+        else:
+            diff -= 1
+            arr[0] += 1
+
+    return arr
+
+
+if __name__ == "__main__":
+    print(get_change(5, 0.99))
+
+
+
+
+
+# get_change(5, 0.09)
+# get_change(5, 0.99)
+
+
+# 0.01, 0.05, 0.10, 0.25, 0.5, 1
