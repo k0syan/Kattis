d635a5651fd7deafcea87a497f3a03a708db28de
Shahen Kosyan <shahen.qosyan@gmail.com>
2019-03-09 20:32:16 +0400

    toilet seat

--- toilet_seat.py
+++ toilet_seat.py
@@ -3,28 +3,34 @@ if __name__ == "__main__":
     r1c = 0
     r2c = 0
     r3c = 0
-    for i in range(len(rules) - 1):
+    curr = rules[0]
+    for i in range(1, len(rules)):
         r = rules[i]
-        nr = rules[i + 1]
 
-        if i == 0:
-            if r != "U":
-                r1c += 1
+        if curr == "D":
+            r1c += 1
+        elif curr == "U" and r == "D":
+            r1c += 2
 
-            if r != "D":
-                r2c += 1
+        curr = "U"
 
-            if r != nr:
-                r3c += 1
-        else:
-            if r != "U":
-                r1c += 2
+    curr = rules[0]
+    for i in range(1, len(rules)):
+        r = rules[i]
 
-            if r != "D":
-                r2c += 2
+        if curr == "U":
+            r2c += 1
+        elif curr == "D" and r == "U":
+            r2c += 2
 
-            if r != nr:
-                r3c += 1
+        curr = "D"
+
+    for i in range(1, len(rules)):
+        pr = rules[i - 1]
+        r = rules[i]
+
+        if r != pr:
+            r3c += 1
 
     print(r1c)
     print(r2c)
